06cfe34a7c59b4f0e5daaabcd75a26f8d11f377c
Jan Willhaus <mail@janwillhaus.de>
2017-02-24 16:43:56 +0100

    Enforce utf-8 readin of dashboard in hapush

--- hapush/hapush.py
+++ hapush/hapush.py
@@ -202,7 +202,7 @@ def readDash(file):
   logger.info("Reading dashboard: %s", file)
 
   with open(file) as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
+    soup = BeautifulSoup(f.read().encode('utf-8', 'ignore'), 'html.parser')
 
   # All divs with data-id are assumed to be widgets
   for div in soup.find_all('div', attrs={'data-id': True}):
